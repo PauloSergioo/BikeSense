840324ba46e5c14d36a1ffe555a634978f5018a5
PauloSergioo <paulosergioilha38@gmail.com>
2025-05-16 23:03:06 -0300

    First commit

---
+++ bikesense/src/test/java/com/bikesense/BikesenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikesense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
